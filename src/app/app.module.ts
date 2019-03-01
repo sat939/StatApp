604e77a60dc8dd560b964ce894a076fa08e0a6bf
sat939 <vsponakala@gmail.com>
2019-02-28 22:46:14 -0600

    adding more styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
 import { HomeComponent } from './Home/home.component';
+import { DescriptionComponent } from './description/description.component';
 // import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
